3c9cdc9c92b739ad1be3dec7dddcf96683002169
vdogic <vdogic@fina.hr>
2022-04-08 19:49:23 +0200

    commit_s_posla

--- solutions/0036516655/lab2py/solution_lab2.py
+++ solutions/0036516655/lab2py/solution_lab2.py
@@ -14,6 +14,7 @@ class Lab2:
         self.resolution_path = ""
         self.clause_dict = {}
         self.nr_of_starting_clauses = int
+        self.nr_of_clauses = int
 
         self.args = sys.argv[1:]
 
@@ -30,14 +31,14 @@ class Lab2:
     def load_resolution(self, resolution_path):
         with open(resolution_path, "r", encoding="utf8") as file:
             file_read = file.readlines()
-            nr_of_lines = sum(1 for x in file_read)
+            self.nr_of_clauses = sum(1 for x in file_read)
             nth_line = 1
 
             for line in file_read:  # PO LINIJI U FILE-U
                 line_elements = line.strip().lower().split("v")
                 literal_set = set()
 
-                if nth_line == nr_of_lines:  # AKO JE ZADNJI RED --> TREBA DE MORGANA
+                if nth_line == self.nr_of_clauses:  # AKO JE ZADNJI RED --> TREBA DE MORGANA
                     self.nr_of_starting_clauses = nth_line - 1
                     for line_element in line_elements:
                         line_element = line_element.strip()
@@ -70,4 +71,48 @@ class Lab2:
 
 
 if __name__ == "__main__":
+    print("aaa")
     run = Lab2()
+
+'''prvi = {
+    1: {"~a", "e", "i"},
+    2: {"b", "d"},
+    3: {"c"},
+    4: {"d", "f"}
+}
+
+drugi = {
+    1: {"a", "e", "u"},
+    2: {"b", "c"},
+    3: {"c", "k"},
+    4: {"d", "l"}
+}
+
+set_of_tuples = {
+    (1, 2),
+    (2, 3)
+}
+
+new_clause = set
+
+char = "~"
+
+print(str(char + "c"))
+
+def custom_intersection(s1, s2):
+    for x in s1:
+        for y in s2:
+            if x == str("~" + y):
+                print("ovo je diff", s1.union(s2).difference(y))
+                #napravimo uniju bez tog chara
+
+for s1 in prvi.values():
+    for s2 in drugi.values():
+        if s1.intersection(s2) != set():
+            if s1 != s2:
+                custom_intersection(s1, s2)
+                #print("{} is from s1[{}] and s2[{}]".format(s1.intersection(s2)))
+            #new_clause = s1.intersection(s2)
+
+print(new_clause)
+'''
